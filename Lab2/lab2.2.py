223fde244813e4bb2e67f3811ef3b492b47bf623
MrOpera102 <vlad051082@gmail.com>
2024-11-01 00:20:55 +0300

    Add files via upload

---
+++ Lab2/lab2.2.py
@@ -0,0 +1,13 @@
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+months = 10  # Количество месяцев, которое планируется протянуть без долгов
+increase = 0.03  # Ежемесячный рост цен
+
+# TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
+all_salary = salary * months
+all_spend = 0
+for _ in range(months):
+    all_spend += spend
+    spend *= increase + 1
+money_capital = all_spend - all_salary
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", round(money_capital))
