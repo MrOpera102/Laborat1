c281b62b47afb9509269c00637157084e36b869b
MrOpera102 <vlad051082@gmail.com>
2024-11-01 21:31:59 +0300

    Update lab2.2.py

--- Lab2/lab2.2.py
+++ Lab2/lab2.2.py
@@ -1,3 +1,5 @@
+import math
+
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 months = 10  # Количество месяцев, которое планируется протянуть без долгов
@@ -10,4 +12,4 @@ for _ in range(months):
     all_spend += spend
     spend *= increase + 1
 money_capital = all_spend - all_salary
-print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", round(money_capital))
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", math.ceil(money_capital))
